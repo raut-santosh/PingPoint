3a33061db95b0412b3617103700d0bd004dd1613
Santosh Raut <rautsantoshtukaram@gmail.com>
2024-11-24 23:47:31 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom"
+import Messages from "./pages/Messages"
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route index={true} element={<Messages />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
