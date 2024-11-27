4371660eee0799e6aaf61ce3ddc6483b09db20ab
Santosh Raut <rautsantoshtukaram@gmail.com>
2024-11-27 16:01:39 +0530

    verification ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import Messages from "./pages/Messages"
 import Login from "./pages/auth/Login"
 import SignUp from "./pages/auth/SignUp"
+import Verification from "./pages/auth/Verification"
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Route index={true} element={<Messages />} />
         <Route path="/auth/login" element={<Login />} />
         <Route path="/auth/signup" element={<SignUp />} />
+        <Route path="/auth/verify" element={<Verification />} />
       </Routes>
     </>
   )
